1bf74e9d0d2ced91dcd3ac30d612eca6544bdfc5
trolllemon <haydnt@zohomail.com>
2022-04-27 17:04:39 -0700

    starts app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // this will hide the console window on Windows
+
+
+
+
+
+mod app;
+mod graph_launcher;
+mod graph;
+use app::UserInput;
+use eframe::{egui::vec2, run_native, NativeOptions};
+use tokio;
+use graph_launcher::spawn;
+
+#[tokio::main]
+async fn main() {
+
+
+
+     let app = UserInput::default();
+     let mut window_options = NativeOptions::default();
+     window_options.initial_window_size = Some(vec2(700.0, 400.0));
+     window_options.resizable = true;
+     run_native(Box::new(app), window_options);
 }
+
