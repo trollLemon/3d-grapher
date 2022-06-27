8aeee05edea6cd703a40af44ddab23ee1c71289d
haydn <haydnt@zohomail.com>
2022-06-26 20:00:07 -0700

    test graph :D

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,41 @@ mod util;
 use app::UserInput;
 use eframe::{egui::vec2, run_native, NativeOptions};
 use tokio;
-
+use fastcurve_3d::fast_curve_2d;
+use gnuplot::Figure;
+use gnuplot::Caption;
+use gnuplot::Color;
 #[tokio::main]
 async fn main() {
     let app = UserInput::default();
     let mut window_options = NativeOptions::default();
     window_options.initial_window_size = Some(vec2(700.0, 400.0));
     window_options.resizable = true;
+
+
+    //following is a visual test and will be removed in the future
+
+    let a = calculator::vector_function_calculator("Sin(t)+t".to_string(), 0);
+    let b = calculator::vector_function_calculator("Sin(t)+t".to_string(), 1);
+    let c = calculator::vector_function_calculator("Sin(t)+t".to_string(), 2);
+    let d = calculator::vector_function_calculator("Sin(t)+t".to_string(), 3);
+    let e = calculator::vector_function_calculator("Sin(t)+t".to_string(), 4);
+    let x = vec![0.0,1.0,2.0,3.0,4.0];
+    let y = vec![a,b,c,d,e];
+    let n: u8 = 4;
+
+    println!("1, {}",a);
+
+     let (xn,yn) = fast_curve_2d(&x, &y, n);
+
+
+    let mut fg = Figure::new();
+    fg.axes2d()
+    .lines(&xn, &yn, &[Caption("Smoothed"), Color("red")]);
+    
+    fg.set_scale(1.0, 1.0);
+    fg.show().unwrap();
+
+
     run_native(Box::new(app), window_options);
 }
