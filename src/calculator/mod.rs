369aa84d9a148c43e3ebebd647cae1d51b4c7003
haydn <haydnt@zohomail.com>
2022-06-27 12:03:25 -0700

    vector calculation function is now used by another to get data

--- src/calculator/mod.rs
+++ src/calculator/mod.rs
@@ -1,11 +1,29 @@
 mod equation_handeler;
 mod math;
 
-pub fn vector_function_calculator(parametric: String, t: i64) -> f64 {
+pub fn vector_function_calculator(parametric:&String, t: i64) -> f64 {
     math::calculate(parametric, t)
 
 
 }
 
-#[cfg(test)]
-mod tests {}
+
+
+pub fn get_some_data(parametric: String) ->Vec<f64> {
+
+    let mut  vector = vec![];
+
+    let lim = 1000;
+
+    let mut count = 0;
+
+    while count < lim {
+    let val = vector_function_calculator(&parametric, count);
+    count +=1;
+    vector.push(val);
+
+    }
+
+    vector
+}
+
