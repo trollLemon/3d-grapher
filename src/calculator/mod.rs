bbc8eb6c859e08ea8536629e1a10c9856e3dec51
haydn <haydnt@zohomail.com>
2022-06-26 19:59:46 -0700

    made this only run once because of ownership problems

--- src/calculator/mod.rs
+++ src/calculator/mod.rs
@@ -1,22 +1,10 @@
 mod equation_handeler;
 mod math;
 
-pub async fn vector_function_calculator(parametric: String) -> Vec<[f64; 2]> {
-    let parsed_eq = equation_handeler::expr(&parametric);
+pub fn vector_function_calculator(parametric: String, t: i64) -> f64 {
+    math::calculate(parametric, t)
 
-    let mut values: Vec<[f64; 2]> = Vec::new();
 
-    let mut counter = 0.0;
-
-    while counter <= 1000.0 {
-        let output = math::do_some_math(parsed_eq.to_string());
-
-        values.push([counter, output]);
-
-        counter += 1.0;
-    }
-
-    values
 }
 
 #[cfg(test)]
