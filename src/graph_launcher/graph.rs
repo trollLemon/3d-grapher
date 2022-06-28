e1ad7147a43e84ba66b75e3dca04b349dac79424
haydn <haydnt@zohomail.com>
2022-06-28 16:37:37 -0700

    spacing

--- src/graph_launcher/graph.rs
+++ src/graph_launcher/graph.rs
@@ -1,26 +1,23 @@
-﻿use crate:: calculator;
+﻿use crate::calculator;
 use fastcurve_3d::fast_curve_3d;
 use gnuplot::{Caption, Color, Figure};
 
-pub fn run(x_eq:String, y_eq:String, z_eq:String) -> Result<(), String> {
-   
+pub fn run(x_eq: String, y_eq: String, z_eq: String) -> Result<(), String> {
     //get some data for all three equations
     let x = calculator::get_some_data(x_eq);
     let y = calculator::get_some_data(y_eq);
     let z = calculator::get_some_data(z_eq);
-    
-    
+
     let n = 8;
 
-    let (xn,yn,zn) = fast_curve_3d(&x, &y, &z, n);
+    let (xn, yn, zn) = fast_curve_3d(&x, &y, &z, n);
 
     // gnu plot Figure
     let mut fg = Figure::new();
     fg.axes3d()
-    .lines(&xn, &yn, &zn, &[Caption("equation"), Color("red")]);
+        .lines(&xn, &yn, &zn, &[Caption("equation"), Color("red")]);
     fg.set_scale(1.0, 1.0);
     fg.show().unwrap();
-
 
     Ok(())
 }
--- src/graph_launcher/mod.rs
+++ src/graph_launcher/mod.rs
@@ -1,8 +1,8 @@
 mod graph;
 use std::io::Error;
 
-pub fn spawn(x :String, y:String, z:String) -> Result<(), Error> {
-    graph::run(x,y,z).unwrap_or_else(|error| {
+pub fn spawn(x: String, y: String, z: String) -> Result<(), Error> {
+    graph::run(x, y, z).unwrap_or_else(|error| {
         println!("um:{}", error);
     });
 
