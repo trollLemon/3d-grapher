59b18c8ea765bbcb347a936d8a335d2603a76789
haydn <haydnt@zohomail.com>
2022-06-27 20:54:20 -0700

    removed function that we dont need anymore

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,59 +3,8 @@
   Any spaces are also removed.
 */
 
-pub fn parse_input(input: &String) -> [String; 3] {
-    let mut indexes_of_commas: Vec<usize> = vec![];
 
-    //iterate over the String and find where the ',' are
-    let bytes = input.as_bytes();
-    let target = b',';
 
-    find_char(bytes, target, &mut indexes_of_commas);
-
-    //now split the String up into thirds now that we know where to slice
-    let x = input[..indexes_of_commas[0]].to_string();
-    let y = input[indexes_of_commas[0] + 1..indexes_of_commas[1]].to_string();
-    let z = input[indexes_of_commas[1] + 1..].to_string();
-
-    //before returning the Array, remove any spaces in the three Strings
-    let target = b' ';
-
-    let x_as_bytes = x.as_bytes();
-    let y_as_bytes = y.as_bytes();
-    let z_as_bytes = z.as_bytes();
-
-    //vectors of all spaces for each string
-
-    let mut x_spaces: Vec<usize> = vec![];
-    let mut y_spaces: Vec<usize> = vec![];
-    let mut z_spaces: Vec<usize> = vec![];
-
-    //since we have three new strings, we can interate through each of them seperatly instead of the whole thing like last time
-    find_char(x_as_bytes, target, &mut x_spaces);
-    find_char(y_as_bytes, target, &mut y_spaces);
-    find_char(z_as_bytes, target, &mut z_spaces);
-
-    //now that we know where the spaces are, we can remove them and return the strings without spaces
-    let fin_x = remove_spaces(&x);
-    let fin_y = remove_spaces(&y);
-    let fin_z = remove_spaces(&z);
-
-    //now we can return the array
-    [fin_x, fin_y, fin_z]
-}
-
-/*helper functions*/
-
-/*
-  Finds the given char in a string(as bytes), and adds its index into a vector
-*/
-fn find_char(bytes: &[u8], target: u8, list: &mut Vec<usize>) {
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == target {
-            list.push(i);
-        }
-    }
-}
 
 pub fn remove_spaces(stringy: &String) -> String {
     let trim_string = stringy.trim();
