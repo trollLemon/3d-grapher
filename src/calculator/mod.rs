cd321a354b22debf8c0e3cc3e854a3801038bd44
haydn <haydnt@zohomail.com>
2022-06-28 21:42:14 -0700

    reduced max t value

--- src/calculator/mod.rs
+++ src/calculator/mod.rs
@@ -13,15 +13,18 @@ pub fn get_some_data(parametric: String) ->Vec<f64> {
     
     let mut  vector = vec![];
 
-    let lim = 100;
+    let lim = 25;
 
     let mut count = 0;
 
     while count < lim {
     let val = vector_function_calculator(&parametric, count);
+    
+    println!("{},{}", &val, &count);
+
     count +=1;
     vector.push(val);
-
+    
     }
 
     vector
